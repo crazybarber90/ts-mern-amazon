1ef9c320153229a347c5a134c70db840f37632e7
crazybarber90 <pepy90aa@gmail.com>
2023-10-28 13:27:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Typescrit MERN AMAZON
---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <header>TS Amazon</header>
+
+        <main></main>
+
+        <footer>All Right Reserved</footer>
+      </div>
+    </>
+  )
+}
+
+export default App
